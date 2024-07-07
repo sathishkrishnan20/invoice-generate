e4cf585c66e5d1ba7528739843048434d25d25d6
sathishkrishnan20 <sathishkrish20@gmail.com>
2024-07-07 10:09:10 +0530

    updated

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 import prisma from "@/libs/prismadb";
 
@@ -9,18 +9,18 @@ export async function POST(request: Request) {
     const body = await request.json();
     const { email, name, password } = body;
    console.log('Response JSON Captured')
-    // const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 10);
     console.log({
       email,
         name,
-        hashedPassword: password,
+        hashedPassword,
     })
     console.log({db: process.env.DATABASE_URL })
     const user = await prisma.user.create({
       data: {
         email,
         name,
-        hashedPassword: password,
+        hashedPassword,
       },
     });
   
