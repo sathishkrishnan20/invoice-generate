870f43e407b798449425fdadae55b0b87feb3870
sathishkrishnan20 <sathishkrish20@gmail.com>
2024-07-07 10:17:37 +0530

    Reverted to old state

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,18 +4,9 @@ import bcrypt from "bcrypt";
 import prisma from "@/libs/prismadb";
 
 export async function POST(request: Request) {
-  try {
-    console.log('Starting to Send the API Register')
     const body = await request.json();
     const { email, name, password } = body;
-   console.log('Response JSON Captured')
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log({
-      email,
-        name,
-        hashedPassword,
-    })
-    console.log({db: process.env.DATABASE_URL })
     const user = await prisma.user.create({
       data: {
         email,
@@ -23,11 +14,5 @@ export async function POST(request: Request) {
         hashedPassword,
       },
     });
-  
     return NextResponse.json(user);
-  } catch (error) {
-    console.log(error)
-    return NextResponse.json(error);
-  }
-
 }
