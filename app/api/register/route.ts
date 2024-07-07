e0ced30e5d0046f7071f8cb5455239b84e286d15
sathishkrishnan20 <sathishkrish20@gmail.com>
2024-07-07 10:05:26 +0530

    logs added

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -5,11 +5,17 @@ import prisma from "@/libs/prismadb";
 
 export async function POST(request: Request) {
   try {
+    console.log('Starting to Send the API Register')
     const body = await request.json();
     const { email, name, password } = body;
-  
+   console.log('Response JSON Captured')
     // const hashedPassword = await bcrypt.hash(password, 12);
-  
+    console.log({
+      email,
+        name,
+        hashedPassword: password,
+    })
+    console.log({db: process.env.DATABASE_URL })
     const user = await prisma.user.create({
       data: {
         email,
